f0602b1c7269f7113f97fc11775fad1931a924f5
KimDaH1 <rlarkgus6859@naver.com>
2023-05-11 20:12:44 +0900

    1. AES-256 암호화 복호화 기능 추가  가. 회원가입시 비밀번호 암호화 하여 저장  나. 로그인시 암호화된 비밀번호를 복호화 하면 입력된 비밀번호와 대조 후 로그인

--- src/main/java/user/UserDao.java
+++ src/main/java/user/UserDao.java
@@ -4,6 +4,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+//암호화 복호화를 위함 import
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
 import user.DBConnectionManager;
 
 public class UserDao {
@@ -11,6 +17,16 @@ public class UserDao {
 	private Connection conn;
 	private PreparedStatement pstmt;
 	private ResultSet rs;
+	
+	private String result;
+	
+	//알고리즘
+	String alg = "AES/CBC/PKCS5Padding";
+	//키
+	String aesKey = "abcdefghabcdefghabcdefghabcdefgh"; //32byte
+
+	String aesIv = "0123456789abcdef"; //16byte
+	
 
 	public UserDao() {
 
@@ -39,7 +55,19 @@ public class UserDao {
 			pstmt.setString(1, userID);
 			rs = pstmt.executeQuery();
 			if(rs.next()) {
-				if(rs.getString(1).equals(userPassword)) 
+				String encrypedPassword = rs.getString(1); //DB에서 암호화된 패스워드를 가져옴
+				// 복호화할 때 사용한 알고리즘, 키 초기화 백터(IV) 정보
+				//알고리즘 aes-256 암호화 해석 코드 **********[복호화]**********
+				Cipher cipher = Cipher.getInstance(alg);
+				SecretKeySpec keySpec = new SecretKeySpec(aesKey.getBytes(), "AES");
+				IvParameterSpec ivParamSpec = new IvParameterSpec(aesIv.getBytes());
+				cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParamSpec);
+				//암호화 해석
+				byte[] decodedPassword = Base64.getDecoder().decode(encrypedPassword);
+				byte[] decryptedPassword = cipher.doFinal(decodedPassword);
+				String decryptedPasswordString = new String(decryptedPassword, "UTF-8");
+
+				if(decryptedPasswordString.equals(userPassword)) 
 					return 1; //로그인성공
 				else
 					return 0; //비밀번호 불일치
@@ -52,7 +80,30 @@ public class UserDao {
 	}
 	
 	public int join(UserDto user) {
+		//암호화 할 유저 패스워드
+		String userPassword = user.getUserPassword();
+		//암호화된 유저 아이디
+		String encPassword = "";
 		
+		try {
+
+			Cipher cipher = Cipher.getInstance(alg);
+
+			//키로 비밀키 생성
+			SecretKeySpec keySpec = new SecretKeySpec(aesKey.getBytes(), "AES");
+			//iv 로 spec 생성
+			IvParameterSpec ivParamSpec = new IvParameterSpec(aesIv.getBytes());
+			//암호화 적용
+			cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParamSpec);
+
+			//암호화 실행
+			byte[] encrypted1 = cipher.doFinal(userPassword.getBytes("UTF-8")); // ID 암호화(인코딩 설정)
+			encPassword = Base64.getEncoder().encodeToString(encrypted1); // 암호화 인코딩 후 저장
+
+		} catch (Exception e) {
+			System.out.println("암호화 중 오류 발생 ");
+			e.printStackTrace();
+		}
 		int result = 0;
 		String SQL = "INSERT INTO TM_USERS (usernumber, userID, userPassword, userName, userGender, userEmail, userPhone)"
 				+ " values((SELECT NVL(MAX(usernumber),0)+1 from tm_users), ?, ?, ?, ?, ?, ?)";
@@ -62,7 +113,7 @@ public class UserDao {
 			
 			pstmt =conn.prepareStatement(SQL);
 			pstmt.setString(1, user.getUserID());
-			pstmt.setString(2, user.getUserPassword());
+			pstmt.setString(2, encPassword);//암호화된 비밀번호 저장
 			pstmt.setString(3, user.getUserName());
 			pstmt.setString(4, user.getUserGender());
 			pstmt.setString(5, user.getUserEmail());
--- src/main/resources/join.sql
+++ src/main/resources/join.sql
@@ -1,14 +1,23 @@
 -- 회원정보 테이블 정보 생성1
 CREATE table TM_USERS
 (
- usernumber number(5) PRIMARY KEY,
- userid varchar2(40),
+ usernumber number(5),
+ userid varchar2(40) PRIMARY KEY ,
  userpassword varchar2(40),
  username varchar2(40),
- usergender CHAR(1) CHECK(GENDER IN ('m,'w')),
+ usergender CHAR(1),
  useremail varchar2(40),
  userphone varchar2(40)
-);
+ );
+
+--테이블 자체 삭제
+drop table tm_users;
+
+--테이블 삭제
+DELETE tm_users;
+
+--회원정보 확인
+select * from tm_users;
 
 -- 테이블 생성하고 들어가는지 확인하는 목적으로 인서트 구문
 INSERT INTO TM_USERS (usernumber, userID, userPassword, userName, userGender, userEmail, userPhone)
